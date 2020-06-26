5489c2c330d0f41e90f57e46de4cf001aa4eafe0
Akshay Raichur <lordakshay99@gmail.com>
2020-06-26 19:18:03 +0530

    Removed

--- README.md
+++
@@ -1,4 +0,0 @@
-# foodshala
-Built a simple food ordering app as an assignment for Internshala Internship selection process. 
-
-#### The app is online here : https://foodshala.akshayraichur.com/
--- backend/Client/src/Screens/Error404.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Error404 = () => {
-  return (
-    <div className="container my-5 pt-5">
-      <h1 className="h1 my-5  pt-5">You have reached an unknown route</h1>
-    </div>
-  );
-};
